88d50e1e30ccdb0deb5bc0ea85870c77086a725f
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-11-23 12:22:47 -0500

    improved dna and wordcount exercises

--- rna-transcription/dna.py
+++ rna-transcription/dna.py
@@ -1,5 +1,2 @@
 def to_rna(dna):
-    rna = ""
-    for c in dna.replace("A", "U").replace("T", "A"):
-        rna += "C" if c == "G" else "G" if c == "C" else c
-    return rna
+    return dna.translate(str.maketrans("GCTA", "CGAU"))
--- word-count/wordcount.py
+++ word-count/wordcount.py
@@ -3,5 +3,5 @@ import regex
 def word_count(text):
     count = {}
     for word in regex.findall(r'[\p{L}\p{N}]+', text.lower()):
-        count[word] = count[word] + 1 if word in count else 1
+        count[word] = count.get(word, 0) + 1
     return count
