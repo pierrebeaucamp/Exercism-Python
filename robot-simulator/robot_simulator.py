05301f5dde2814af0364bddbc15247e73d652836
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-11-25 16:52:08 -0500

    Submited robot-simulation, improved on series.py

---
+++ robot-simulator/robot_simulator.py
@@ -0,0 +1,29 @@
+import itertools
+
+NORTH = (0, 1)
+EAST = (1, 0)
+SOUTH = (0, -1)
+WEST = (-1, 0)
+
+class Robot:
+    def __init__(self, b=NORTH, x=0, y=0):
+        self.coordinates = (x, y)
+        self.__directions = itertools.cycle([NORTH, EAST, SOUTH, WEST])
+        self.turn_right()
+        while self.bearing != b: self.turn_right()
+
+    def advance(self):
+        self.coordinates = tuple(map(sum,zip(self.coordinates, self.bearing)))
+
+    def simulate(self, command):
+        for c in command:
+            if c == "L": self.turn_left()
+            if c == "R": self.turn_right()
+            if c == "A": self.advance()
+
+    def turn_left(self):
+        for _ in range(3): self.turn_right()
+
+    def turn_right(self):
+        self.bearing = next(self.__directions)
+        print(self.bearing)
--- series/series.py
+++ series/series.py
@@ -1,6 +1,5 @@
 def slices(number, n):
+    if not n or len(number) < n: raise ValueError("No match possible")
     sub = lambda number, n: [int(number[i]) for i in range(n)]
-    out = [sub(number[i:], n) for i in range(len(number) + 1 - n)]
-    if not out or not n: raise ValueError("No match possible")
-    return out
+    return [sub(number[i:], n) for i in range(len(number) + 1 - n)]
 
