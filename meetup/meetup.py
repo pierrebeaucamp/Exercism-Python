08c502ed0f92bd67a23d7d2341ffb2f10f80fd8d
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-11-24 21:24:10 -0500

    Submitted meetup exercise

---
+++ meetup/meetup.py
@@ -0,0 +1,22 @@
+import calendar
+import datetime
+import regex
+
+def meetup_day(year, month, day, label):
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+            "Sunday"]
+
+    try: delta = int(regex.findall(r'\d', label)[0])
+    except IndexError: delta = -1 if label == "last" else 1
+
+    # itermonthdays will return 0's if a day isn't present in a month. We need
+    # to remove those 0's from the list!
+    for d in filter((0).__ne__, calendar.Calendar().itermonthdays(year, month)):
+        if label == "teenth" and (d < 13 or d > 19): continue
+        if datetime.date(year, month, d).weekday() == week.index(day):
+            date = datetime.date(year, month, d)
+            delta -= 1
+        if delta == 0: break
+
+    if delta > 0: raise ValueError("Day is not exisiting")
+    return date
