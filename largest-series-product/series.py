d0a20d29870b737ade36efa486a9e7336eeac22e
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-11-26 18:46:11 -0500

    improved previous exercises

--- largest-series-product/series.py
+++ largest-series-product/series.py
@@ -5,5 +5,4 @@ def largest_product(number, n):
 
 def slices(number, n):
     if len(number) < n: raise ValueError("No match possible")
-    sub = lambda number, n: [int(number[i]) for i in range(n)]
-    return [sub(number[i:], n) for i in range(len(number) + 1 - n)]
+    return [[int(x) for x in number[i:i+n]] for i in range(len(number) + 1 - n)]
--- series/series.py
+++ series/series.py
@@ -1,5 +1,4 @@
 def slices(number, n):
     if not n or len(number) < n: raise ValueError("No match possible")
-    sub = lambda number, n: [int(number[i]) for i in range(n)]
-    return [sub(number[i:], n) for i in range(len(number) + 1 - n)]
+    return [[int(x) for x in number[i:i+n]] for i in range(len(number) + 1 - n)]
 
