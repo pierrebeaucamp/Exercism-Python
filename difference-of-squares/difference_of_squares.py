910b764c6726e9f131fb3a394c70d9b5bb167be9
Pierre Beaucamp <mail@pierrebeaucamp.com>
2016-01-10 21:00:08 -0500

    Improved my original difference of squares exercise

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,8 +1,8 @@
 def square_of_sum(x):
-    return sum(range(x+1))**2
+    return (x * (x + 1) / 2) ** 2
 
 def sum_of_squares(x):
-    return sum(i**2 for i in range(x+1))
+    return sum(i ** 2 for i in range(x + 1))
 
 def difference(x):
     return square_of_sum(x) - sum_of_squares(x)
